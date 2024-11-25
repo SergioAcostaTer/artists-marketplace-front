62bb02caa27aca962d8c423e4983d32644cccbee
Sergio Acosta <sergioacostaquintana@gmail.com>
2024-11-25 18:52:41 +0100

    redirect locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
 import { routing } from "@/i18n/routing";
 import createMiddleware from "next-intl/middleware";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 const intlMiddleware = createMiddleware(routing);
 
 export default function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-  const { pathname } = url;
+  // const url = req.nextUrl.clone();
+  // const { pathname } = url;
 
-  const hasLocalePrefix = /^\/(es|en|fr)\//.test(pathname);
+  // const hasLocalePrefix = /^\/(es|en|fr)\//.test(pathname);
 
-  console.log("hasLocalePrefix", hasLocalePrefix);
+  // console.log("hasLocalePrefix", hasLocalePrefix);
 
-  if (!hasLocalePrefix && pathname !== "/") {
-    const url = new URL(process.env.NEXT_PUBLIC_BASE_URL + "/en" + pathname);
+  // if (!hasLocalePrefix && pathname !== "/") {
+  //   const url = new URL(process.env.NEXT_PUBLIC_BASE_URL + "/en" + pathname);
 
-    console.log("redirecting to", url.toString());
+  //   console.log("redirecting to", url.toString());
 
-    return NextResponse.redirect(url.toString(), { status: 302 });
-  }
+  //   return NextResponse.redirect(url.toString(), { status: 302 });
+  // }
 
   return intlMiddleware(req);
 }
