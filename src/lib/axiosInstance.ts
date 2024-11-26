acf6b8dad657268c292fdcf75379ec08a0b49e46
Sergio Acosta <sergioacostaquintana@gmail.com>
2024-11-26 09:34:44 +0100

    deployed

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: true ? "api" : "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
