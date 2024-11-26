7784e93f9487a2d2edd1fb1d7125c0835f3a7f25
Sergio Acosta <sergioacostaquintana@gmail.com>
2024-11-26 09:49:00 +0100

    deployed

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: false ? "https://studiohub.es/api" : "http://localhost:8000/api",
+  baseURL: true ? "https://studiohub.es/api" : "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
