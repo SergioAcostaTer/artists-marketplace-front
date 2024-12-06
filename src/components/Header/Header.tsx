a7c4d894a6d8a3169162414548fd28d3db8e10fd
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2024-12-06 17:51:11 +0100

    link logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import MenuMobile from "./MenuMobile";
 import SearchInput from "./SearchInput";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -9,14 +10,16 @@ export default function Header() {
       <div className="h-[64px]" />
       <header className="p-4 bg-background text-white h-[64px] fixed top-0 z-[1000] gap-2 grid grid-cols-[auto,1fr,auto] items-center w-full border-b border-[#ffffff87]">
         <div className="flex justify-between items-center">
-          <Image
-            src="/logos/vice.webp"
-            alt="Logo"
-            width={80}
-            height={80}
-            className="w-20 object-contain"
-            priority={true}
-          />
+          <Link href="/">
+            <Image
+              src="/logos/vice.webp"
+              alt="Logo"
+              width={80}
+              height={80}
+              className="w-20 object-contain"
+              priority={true}
+            />
+          </Link>
         </div>
         <div>
           <SearchInput />
